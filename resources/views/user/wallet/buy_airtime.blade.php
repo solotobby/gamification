65e418da0271540b5ad73e939457158fdce9a76b
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2022-09-08 08:43:35 +0100

    fixex

--- resources/views/user/wallet/buy_airtime.blade.php
+++ resources/views/user/wallet/buy_airtime.blade.php
@@ -1,13 +1,28 @@
 @extends('layouts.main.master')
 @section('content')
+  <!-- Page Content -->
+<div class="content">
+  <div class="block block-rounded">
+    <div class="block-header block-header-default">
+      <h3 class="block-title">Airtime</h3>
+    </div>
+  <div class="block-content">
+ 
+    <form class="js-validation-reminder" action="{{ route('buy.airtime') }}" method="POST">
+        @csrf
+      <!-- Text -->
 
+      <div class="mb-2 text-center content-heading mb-4">
+        <p class="text-uppercase fw-bold fs-sm text-muted">Buy Airtime</p>
+        <p class="link-fx fw-bold fs-1">
+          &#8358;{{ number_format(auth()->user()->wallet->balance) }}
+        </p>
+        <p>Wallet Balance</p>
+      </div>
 
- <!-- Page Content -->
- <div class="row g-0 justify-content-center bg-black-75">
-    <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
-      <!-- Reminder Block -->
-      <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
-        <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
+      <div class="row">
+        <div class="col-lg-3"></div>
+          <div class="col-lg-6">
             @if (session('success'))
             <div class="alert alert-success" role="alert">
                 {{ session('success') }}
@@ -29,58 +44,40 @@
                     </ul>
                 </div>
             @endif
-  
-            <!-- Header -->
-          <div class="mb-2 text-center">
-            <p class="text-uppercase fw-bold fs-sm text-muted">Buy Airtime</p>
-            <p class="link-fx fw-bold fs-1">
-              &#8358;{{ number_format(auth()->user()->wallet->balance) }}
-            </p>
-            <p>Wallet Balance</p>
-          </div>
-          <!-- END Header -->
-
-          <!-- Reminder Form -->
-          <form class="js-validation-reminder" action="{{ route('buy.airtime') }}" method="POST">
-              @csrf
             <div class="mb-4">
-              <div class="input-group input-group-lg">
+              <div class="input-group">
+                <span class="input-group-text">
+                  &#8358;
+                </span>
+                {{-- <input type="text" class="form-control text-center" id="example-group1-input3" name="example-group1-input3" placeholder="00"> --}}
                 <input type="number" class="form-control @error('amount') is-invalid @enderror" id="reminder-credential" min="50" max="100" name="amount" value="{{ old('amount') }}" placeholder="Enter Amount" required>
+                {{-- <span class="input-group-text">,00</span> --}}
               </div>
-                @error('amount')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
             </div>
+
             <div class="mb-4">
-                <div class="input-group input-group-lg">
-                  <input type="text" class="form-control @error('phone') is-invalid @enderror" id="reminder-credential" name="phone" value="{{ old('phone') }}" placeholder="Enter phone Number" required>
-                </div>
-                @error('phone')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+              <div class="input-group">
+                <span class="input-group-text">
+                  Number
+                </span>
+                <input type="text" class="form-control @error('phone') is-invalid @enderror" id="reminder-credential" name="phone" value="{{ old('phone') }}" placeholder="Enter phone Number" required>
+                {{-- <input type="text" class="form-control" id="example-group1-input1" name="example-group1-input1"> --}}
               </div>
+            </div>
             <div class="text-center mb-4">
               <button type="submit" class="btn btn-primary">
                 <i class="fa fa-fw fa-reply opacity-50 me-1"></i> Buy Airtime
               </button>
 
             </div>
-           
-          </form>
-          <!-- END Reminder Form -->
-        </div>
-        
+          </div>
+          <div class="col-lg-3"></div>
+       
       </div>
-      
-      <!-- END Reminder Block -->
-    </div>
+      <!-- END Text -->
+    </form>
   </div>
-<!-- END Page Content -->
-
+</div>
 
 @endsection
 
