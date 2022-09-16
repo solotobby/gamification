b035930791afad8523674de3ae2043928ff71165
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2022-09-16 01:10:44 +0100

    fix

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -191,8 +191,8 @@
                           </h3>
 
                           <h4 class="fw-normal text-muted text-center">
-                            verify your account to enjoy &#8358;1,000 user bonus and unlimited access to withdraw funds. <br>
-                            When you refer up to 50 friends, you will earn &#8358;12,500 plus &#8358;5,000 extra bonus from Freebyz.com.
+                            verify your account to enjoy &#8358;1,000 user bonus and unlimited access to withdraw funds. When you refer up to 50 friends, 
+                            you will earn &#8358;12,500 plus &#8358;5,000 extra bonus from us. <br>Got Payment Issues, transfer to 1014763749 - DOMINAHL TECH SERVICES (ZENITH) then send us a mail- info@dominahl.com
                           </h4>
 
                           <center>
