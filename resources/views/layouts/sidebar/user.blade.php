45376a85b21fe70e8cb6ec32718b141d600d1aef
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-03-03 07:49:52 +0000

    temp removed

--- resources/views/layouts/sidebar/user.blade.php
+++ resources/views/layouts/sidebar/user.blade.php
@@ -135,12 +135,12 @@
           <span class="nav-main-link-name">Buy Airtime</span>
         </a>
       </li>
-      <li class="nav-main-item">
+      {{-- <li class="nav-main-item">
         <a class="nav-main-link" href="{{ route('databundle') }}">
           <i class="nav-main-link-icon fa fa-tty"></i>
           <span class="nav-main-link-name">Buy DataBundle</span>
         </a>
-      </li>
+      </li> --}}
       
       <li class="nav-main-item">
         <a class="nav-main-link" href="{{ route('transactions') }}">
