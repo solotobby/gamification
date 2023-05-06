5f7c2ea10c5daa80b6fbb3704bcdf7c00fc88095
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-05-06 14:26:18 +0100

    corrected typo error

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -78,8 +78,7 @@
           
          
           <p style="color:brown">
-            We'll reward you with &#8358;35,000 when you refer 100 verified users and &#8358;350,000 when you refer 1,000 verified users.
-            {{-- <br>Copy your referral link ab to start earning big! --}}
+            We'll reward you with ₦55,000($78) when you refer 100 verified users and ₦510,000 ($728) when you refer 1,000 verified users.
           </p>
           {{-- <p>
             <a href="{{ route('assign.virtual.account') }}" class="btn btn-primary">Get Virtual Account</a>
