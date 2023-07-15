247e8184652fb5255a16b2db76bcf8aff2de7ec1
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-07-15 20:49:00 +0100

    changed ui

--- resources/views/user/wallet/fund.blade.php
+++ resources/views/user/wallet/fund.blade.php
@@ -1,45 +1,84 @@
 @extends('layouts.main.master')
 @section('content')
- <!-- Page Content -->
-    <div class="row g-0 justify-content-center bg-black-75">
-      <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
-        <!-- Reminder Block -->
-        <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
-          <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
-            <!-- Header -->
-            <div class="mb-2 text-center">
-                <p class="text-uppercase fw-bold fs-sm text-muted">Fund Wallet</p>
-                <p class="link-fx fw-bold fs-1">
-                    &#8358;{{ number_format(auth()->user()->wallet->balance) }}
-                </p>
-                <p>Wallet Balance</p>
-                {{-- Account Balance: (&#8358;{{$balance}})
-                Account Number: {{auth()->user()->accountInfo->account_number}}<br>
-                Bank Name: {{auth()->user()->accountInfo->account_name}}  --}}
-            </div>
-            <!-- END Header -->
-
-            <!-- Reminder Form -->
-            <form class="js-validation-reminder" action="{{ route('store.funds') }}" method="POST">
-                @csrf
-              <div class="mb-4">
-                <div class="input-group input-group-lg">
-                  <input type="number" class="form-control" id="reminder-credential" name="balance" min="500" placeholder="Enter Amount" required>
-                </div>
-              </div>
-              <div class="text-center mb-4">
-                <button type="submit" class="btn btn-primary">
-                  <i class="fa fa-fw fa-money-bill opacity-50 me-1"></i> Fund Wallet
-                </button>
-              </div>
-            </form>
-            <!-- END Reminder Form -->
-          </div>
-        </div>
-        <!-- END Reminder Block -->
-      </div>
+<div class="content">
+  <div class="block block-rounded">
+    <div class="block-header block-header-default">
+      <h3 class="block-title">Fund Wallet</h3>
     </div>
-  <!-- END Page Content -->
+  <div class="block-content">
+ 
+    <form class="js-validation-reminder" action="{{ route('store.funds') }}" method="POST">
+        @csrf
+      <!-- Text -->
+
+      <div class="mb-2 text-center content-heading mb-4">
+        <p class="text-uppercase fw-bold fs-sm text-muted">Fund Wallet</p>
+        <p class="link-fx fw-bold fs-1">
+          &#8358;{{ number_format(auth()->user()->wallet->balance) }}
+        </p>
+        <p>Wallet Balance</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3"></div>
+          <div class="col-lg-6">
+            @if (session('success'))
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+            @endif
+
+            @if (session('error'))
+            <div class="alert alert-danger" role="alert">
+                {{ session('error') }}
+            </div>
+            @endif
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            {{-- <div class="mb-4">
+              <div class="input-group">
+               <select class="form-control" name="network" required>
+                  <option value="">Select One</option>
+                  <option value="MTN">MTN</option>
+                  <option value="AIRTEL">AIRTEL</option>
+                  <option value="GLO">GLO</option>
+                  <option value="9MOBILE">9MOBILE</option>
+               </select>
+              </div>
+            </div> --}}
+
+            <div class="mb-4">
+              <div class="input-group">
+                <span class="input-group-text">
+                  &#8358;
+                </span>
+                <input type="number" class="form-control @error('balance') is-invalid @enderror" id="reminder-credential" name="balance" min="500" value="{{ old('balance') }}" placeholder="Enter Amount" required>
+                <span class="input-group-text">.00</span>
+              </div>
+            </div>
+
+            <div class="text-center mb-4">
+              <button type="submit" class="btn btn-primary">
+                <i class="si si-paper-plane opacity-50 me-1"></i> Fund Wallet
+              </button>
+            </div>
+          </div>
+          <div class="col-lg-3"></div>
+       
+      </div>
+      <!-- END Text -->
+    </form>
+  </div>
+</div>
 
 @endsection
 
--- resources/views/user/wallet/withdraw.blade.php
+++ resources/views/user/wallet/withdraw.blade.php
@@ -1,70 +1,106 @@
 @extends('layouts.main.master')
 @section('content')
-
-
- <!-- Page Content -->
- <div class="row g-0 justify-content-center bg-black-75">
-    <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
-      <!-- Reminder Block -->
-      <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
-        <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
-            @if (session('success'))
-            <div class="alert alert-success" role="alert">
-                {{ session('success') }}
-            </div>
-            @endif
+  <!-- Page Content -->
+  <div class="content">
+    <div class="block block-rounded">
+      <div class="block-header block-header-default">
+        <h3 class="block-title">Withdraw Funds</h3>
+      </div>
+    <div class="block-content">
+   
+      <form class="js-validation-reminder" action="{{ route('store.withdraw') }}" method="POST">
+          @csrf
+        <!-- Text -->
   
-            <!-- Header -->
-          <div class="mb-2 text-center">
-            <p class="text-uppercase fw-bold fs-sm text-muted">Withdraw Funds</p>
-            <p class="link-fx fw-bold fs-1">
-              &#8358;{{ number_format(auth()->user()->wallet->balance) }}
-            </p>
-            <p>Wallet Balance</p>
-          </div>
-          <!-- END Header -->
-
-          <!-- Reminder Form -->
-          <form class="js-validation-reminder" action="{{ route('store.withdraw') }}" method="POST">
-              @csrf
+        <div class="mb-2 text-center content-heading mb-4">
+          <p class="text-uppercase fw-bold fs-sm text-muted">Withdraw Funds</p>
+          <p class="link-fx fw-bold fs-1">
+            &#8358;{{ number_format(auth()->user()->wallet->balance) }}
+          </p>
+          <p>Wallet Balance</p>
+        </div>
+  
+        <div class="row">
+          <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+              @if (session('success'))
+              <div class="alert alert-success" role="alert">
+                  {{ session('success') }}
+              </div>
+              @endif
+  
+              @if (session('error'))
+              <div class="alert alert-danger" role="alert">
+                  {{ session('error') }}
+              </div>
+              @endif
+  
+              @if ($errors->any())
+                  <div class="alert alert-danger">
+                      <ul>
+                          @foreach ($errors->all() as $error)
+                              <li>{{ $error }}</li>
+                          @endforeach
+                      </ul>
+                  </div>
+              @endif
+  
+              {{-- <div class="mb-4">
+                <div class="input-group">
+                 <select class="form-control" name="network" required>
+                    <option value="">Select One</option>
+                    <option value="MTN">MTN</option>
+                    <option value="AIRTEL">AIRTEL</option>
+                    <option value="GLO">GLO</option>
+                    <option value="9MOBILE">9MOBILE</option>
+                 </select>
+                </div>
+              </div> --}}
+  
               <div class="mb-4">
-                <div class="input-group input-group-lg">
+                <div class="input-group">
+                  <span class="input-group-text">
+                    &#8358;
+                  </span>
+                  <input type="number" class="form-control @error('amount') is-invalid @enderror" id="reminder-credential" min="500" name="balance" value="{{ old('balance') }}" placeholder="Enter Amount" required>
+                  <span class="input-group-text">.00</span>
+                </div>
+              </div>
+  
+              <div class="mb-4">
+                <div class="input-group">
+                  <span class="input-group-text">
+                    <i class="fa fa-grip-vertical"></i>
+                  </span>
                   <select name="type" class="form-control" required>
                     <option value="">Select An Option</option>
                     <option value="local_withdrawal">Local Withdrawal</option>
                     <option value="paypal_withdrawal">Paypal Withdrawal</option>
                   </select>
                 </div>
-              </div>  
-            <div class="mb-4">
-              <div class="input-group input-group-lg">
-                <input type="number" class="form-control" id="reminder-credential" min="500" name="balance" placeholder="Enter Amount" required>
+              </div>
+              <div class="text-center mb-4">
+               
+                @if(auth()->user()->is_verified == true)
+                <button type="submit" class="btn btn-primary">
+                  <i class="si si-paper-plane opacity-50 me-1"></i> Withdraw Fund
+                </button>
+                @else
+                <button type="button" class="btn btn-primary disabled">
+                  <i class="si si-paper-plane opacity-50 me-1"></i> Withdrawal Not Activated
+                </button>
+                @endif
+  
               </div>
             </div>
-            <div class="text-center mb-4">
-                @if(auth()->user()->is_verified == true)
-              <button type="submit" class="btn btn-primary">
-                <i class="fa fa-fw fa-reply opacity-50 me-1"></i> Withdraw Fund
-              </button>
-              @else
-              <button type="button" class="btn btn-primary disabled">
-                <i class="fa fa-fw fa-reply opacity-50 me-1"></i> Withdrawal Not Activated
-              </button>
-              @endif
-            </div>
-           
-          </form>
-          <p style="color: brown"><i>Note: Withdrawals are made every friday of the week. Only verified users can withdraw</i> </p>
-          <!-- END Reminder Form -->
+            <center><p style="color: brown"><i>Note: Withdrawals are made every friday of the week. Only verified users can withdraw</i> </p></center>
+            <div class="col-lg-3"></div>
+         
         </div>
-        
-      </div>
-      
-      <!-- END Reminder Block -->
+        <!-- END Text -->
+      </form>
     </div>
   </div>
-<!-- END Page Content -->
-
 
 @endsection
 
