1b2b15c4d2e72903429f04a83b47893f0354b084
Oluwatobi Solomon <oluwatobisolomon@pc-139-105.customer.ask4.lan>
2023-08-08 12:40:31 +0100

    implemented sqs

--- config/queue.php
+++ config/queue.php
@@ -55,7 +55,7 @@ return [
             'driver' => 'sqs',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
+            'prefix' => env('AWS_SQS_QUEUE_URL', 'https://sqs.us-east-2.amazonaws.com/062887347336'),
             'queue' => env('SQS_QUEUE', 'default'),
             'suffix' => env('SQS_SUFFIX'),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
