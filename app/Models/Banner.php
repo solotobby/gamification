ded9baaae55fed417cd0d520a5eaeba1ccfd8d78
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-10-10 16:11:27 +0100

    fixed

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'banner_id', 'banner_url', 'external_link', 'age_bracket', 'ad_placement_point', 'adplacement_position', 'duration', 'country', 'amount', 'status'];
 }
--- database/migrations/2023_10_08_194115_create_banners_table.php
+++ database/migrations/2023_10_08_194115_create_banners_table.php
@@ -15,6 +15,17 @@ class CreateBannersTable extends Migration
     {
         Schema::create('banners', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('banner_id');
+            $table->string('banner_url');
+            $table->string('external_link');
+            $table->string('age_bracket');
+            $table->string('ad_placement_point');
+            $table->string('adplacement_position');
+            $table->string('duration');
+            $table->string('country');
+            $table->decimal('amount', 10, 2);
+            $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
