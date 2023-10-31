a5493f368e300d8e5f9c2b658638aa1a5e9a70d4
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-10-31 11:43:04 +0000

    debugging

--- app/Http/Controllers/OTPController.php
+++ app/Http/Controllers/OTPController.php
@@ -22,7 +22,7 @@ class OTPController extends Controller
 
         $phone_number = '234'.substr($request->phone_number, 1);
          
-        $response =  sendOTP($phone_number);
+        return $response =  sendOTP($phone_number);
 
         OTP::create(['user_id' => auth()->user()->id, 'pinId' => $response['pin_id'], 'otp' => '11111', 'phone_number' => $response['to'], 'is_verified' => false]);
 
