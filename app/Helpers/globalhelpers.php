2f16a5d5f88f8ada9f1bd7c331a630336dfcbfc2
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-10-31 11:36:33 +0000

    changed otp api

--- app/Helpers/globalhelpers.php
+++ app/Helpers/globalhelpers.php
@@ -495,29 +495,29 @@ if(!function_exists('sendOTP')){
     function sendOTP($phone){
         
         $payload = [
-            "api_key" => env('TERMI_KEY'),
-                "pin_type" => "NUMERIC",
-                "phone_number" => $phone,
-                "pin_attempts" => 3,
-                "pin_time_to_live" => 60,
-                "pin_length" => 6
             // "api_key" => env('TERMI_KEY'),
-            //  "message_type" => "NUMERIC",
-            //  "to" => $phone,
-            //  "from" => "FREEBYZ",
-            //  "channel" => "dnd",
-            //  "pin_attempts" => 3,
-            //  "pin_time_to_live" =>  5,
-            //  "pin_length" => 6,
-            //  "pin_placeholder" => "< 1234 >",
-            //  "message_text" => "Your Freebyz OTP pin is < 1234 >",
-            //  "pin_type" => "NUMERIC"
+            //     "pin_type" => "NUMERIC",
+            //     "phone_number" => $phone,
+            //     "pin_attempts" => 3,
+            //     "pin_time_to_live" => 60,
+            //     "pin_length" => 6
+            "api_key" => env('TERMI_KEY'),
+             "message_type" => "NUMERIC",
+             "to" => $phone,
+             "from" => "FREEBYZ",
+             "channel" => "dnd",
+             "pin_attempts" => 3,
+             "pin_time_to_live" =>  5,
+             "pin_length" => 6,
+             "pin_placeholder" => "< 1234 >",
+             "message_text" => "Your Freebyz OTP pin is < 1234 >",
+             "pin_type" => "NUMERIC"
         ];
         
         $res = Http::withHeaders([
             'Accept' => 'application/json',
             'Content-Type' => 'application/json',
-        ])->post('https://termii.com/api/sms/otp/generate', $payload);
+        ])->post('https://api.ng.termii.com/api/sms/otp/send', $payload);
         
          return json_decode($res->getBody()->getContents(), true);
 
