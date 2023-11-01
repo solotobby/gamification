91465e5c54b32feb7452b24b1af89d4c0d07ae27
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-11-01 22:25:06 +0000

    test webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -17,7 +17,7 @@ class WebhookController extends Controller
         $event = $request['event'];
 
         if($event == 'charge.success'){
-            $amount = $request['data']['amount'] / 100;
+            $amount = $request['data']['amount']/100;
             $status = $request['data']['status'];
             $reference = $request['data']['reference'];
             $channel = $request['data']['channel'];
@@ -25,31 +25,31 @@ class WebhookController extends Controller
             $email = $request['data']['customer']['email'];
             $customer_code = $request['data']['customer']['customer_code'];
 
-            // $virtualAccount = VirtualAccount::where('customer_id', $customer_code)->first();
+            $virtualAccount = VirtualAccount::where('customer_id', $customer_code)->first();
 
-            // $user = User::where('id', $virtualAccount->user_id)->first();
+            $user = User::where('id', $virtualAccount->user_id)->first();
 
-            // creditWallet($user, 'Naira', $amount);
+            creditWallet($user, 'Naira', $amount);
 
             
-            // PaymentTransaction::create([
-            //     'user_id' => $user->id,
-            //     'campaign_id' => '1',
-            //     'reference' => $reference,
-            //     'amount' => $amount,
-            //     'status' => $status,
-            //     'currency' => $currency,
-            //     'channel' => $channel,
-            //     'type' => 'cash_transfer_top',
-            //     'description' => 'Cash transfer from '.$user->name,
-            //     'tx_type' => 'Credit',
-            //     'user_type' => 'regular'
-            // ]);
+            PaymentTransaction::create([
+                'user_id' => $user->id,
+                'campaign_id' => '1',
+                'reference' => $reference,
+                'amount' => $amount,
+                'status' => $status,
+                'currency' => $currency,
+                'channel' => $channel,
+                'type' => 'cash_transfer_top',
+                'description' => 'Cash transfer from '.$user->name,
+                'tx_type' => 'Credit',
+                'user_type' => 'regular'
+            ]);
 
-            // return response()->json(['status' => 'success']);
+            return response()->json(['status' => 'success'], 200);
 
         }else{
-            return response()->json(['status' => 'error']);
+            return response()->json(['status' => 'error'], 500);
         }
 
     }
