3683fb6d2348b2a62a277fd947d384b2b3764445
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2023-12-01 10:30:06 +0000

    UPDATED BANNER AD LIST

--- resources/views/admin/banner/index.blade.php
+++ resources/views/admin/banner/index.blade.php
@@ -90,13 +90,82 @@
                     </td>
                    
                     <td>
-                      @if($banner->status == true)
-                          <button class="btn btn-secondary btn-sm disabled"> {{ $banner->live_state}}</button>
-                      @else
-                          <a href="{{ url('admin/banner/activate/'.$banner->id) }}" class="btn btn-secondary btn-sm">Take Live</a>
-                      @endif
+                     
+
+                      <button type="button" class="btn btn-alt-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal-default-popout-{{ $banner->id }}">View</button>
                     </td>
                 </tr>
+
+
+                <div class="modal fade" id="modal-default-popout-{{ $banner->id }}" tabindex="-1" role="dialog" aria-labelledby="modal-default-popout" aria-hidden="true">
+                  <div class="modal-dialog modal-lg modal-dialog-popout" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                      <h5 class="modal-title">Details</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+
+                      <div class="modal-body pb-1">
+                          <div class="col-xl-12">
+                              <!-- With Badges -->
+                              <div class="block block-rounded">
+                                <div class="block-header block-header-default">
+                                  <h3 class="block-title">{{ $banner->banner_id }}</h3>
+                                </div>
+                                <div class="block-content">
+                                  
+                                  <img src="{{ url($banner->banner_url)  }}" width="100%" height="300" class="img-responsive mb-4">
+                                  
+                                  <ul class="list-group push">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                      @if($banner->currency == 'NGN')
+                                         Amount -  &#8358;{{ number_format($banner->amount,2) }}
+                                      @else
+                                         Amount - {{ number_format($banner->amount,2) }}
+                                      @endif
+                                       
+                                     </li>
+
+                                     <li class="list-group-item d-flex justify-content-between align-items-center">
+                                      Creator - {{ $banner->user->name}}
+                                     </li>
+
+                                     <li class="list-group-item d-flex justify-content-between align-items-center">
+                                      Clicks -  {{$banner->click_count == null ? '0' : $banner->click_count}}/{{$banner->clicks}}
+                                     </li>
+                                     <li class="list-group-item d-flex justify-content-between align-items-center">
+                                      Impression -  {{$banner->impression}}
+                                     </li>
+
+                                     <li class="list-group-item d-flex justify-content-between align-items-center">
+                                      Status -   {{ $banner->live_state == null ? 'Under Review' : $banner->live_state .' on '. \Carbon\Carbon::parse($banner->banner_end_date)->format('d F, Y') }}
+                                     </li>
+
+                                     <li class="list-group-item d-flex justify-content-between align-items-center">
+                                     Date Created -   {{ \Carbon\Carbon::parse($banner->date)->format('d F, Y') }}
+                                     </li>                                    
+                                  </ul>
+
+                                   @if($banner->status == true)
+                                        <button class="btn btn-secondary btn-sm disabled"> {{ $banner->live_state}}</button>
+                                    @else
+                                        <a href="{{ url('admin/banner/activate/'.$banner->id) }}" class="btn btn-secondary btn-sm">Take Live</a>
+                                    @endif 
+
+                                </div>
+                              </div>
+                              <!-- END With Badges -->
+                            </div>
+                          
+                      </div>
+                      
+                      <div class="modal-footer">
+                      <button type="button" class="btn btn-sm btn-alt-secondary" data-bs-dismiss="modal">Close</button>
+                      {{-- <button type="submit" class="btn btn-sm btn-primary" data-bs-dismiss="modal">Done</button> --}}
+                      </div>
+                  </div>
+                  </div>
+              </div>
                 @endforeach
               
             </tbody>
