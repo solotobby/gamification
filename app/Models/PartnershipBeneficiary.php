e95d7bf7305ac324e108b1d8fdd46b813138b615
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-02-05 12:26:19 +0000

    fixed

--- app/Models/PartnershipBeneficiary.php
+++ app/Models/PartnershipBeneficiary.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class PartnershipBeneficiary extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['partnership_subscriptions_id', 'firstName', 'lastName', 'email', 'phone', 'dateOfBirth', 'gender'];
+    
 }
