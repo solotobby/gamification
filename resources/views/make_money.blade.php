aadd8d834594ee44b50e7b24c7be9011673233c4
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-03-05 20:04:19 +0000

    updates

--- resources/views/make_money.blade.php
+++ resources/views/make_money.blade.php
@@ -23,17 +23,27 @@
 				<div class="row">
 					<div class="col-sm-12 col-md-12 col-lg-12 mb-40">
 						<div class="contact-person">
-							<h4>Four ways you can make money on Freebyz</h4>
+							<h4>How to make money online on Freebyz</h4>
 							<p>
-                                1.	You can complete tasks and earn. As a verified user, you have access to limitless jobs above N10 <br>
-                                2.	You can hire workers to engage your social media posts to earn on Youtube, TikTok, etc <br>
-                                3.	You can invite your friends using your referral link and earn N500 on each verified friend. Some users have cashed out more than 10,000 at a time by just referring friends and not completing tasks at all.
-                                Hurry now to invite your friends and earn big time.  <br>
-                                4.	You automatically earn login points each day you login and copy your referral code to social media. These points can be used for airtime, data or even withdrawal. Feel free to use any of these methods to earn big time on Freebyz. <br>
-                                
-                                Enjoy Life with Freebyz
+                                Freebyz was created for you to make cool cash every day by doing simple social media jobs on YouTube, Facebook, TikTok, Instagram, and other social media platforms. 
+                                Freebyz is 100% legit and many users have earned up to 300k and more.
+                                On Freebyz, you can answer survey questions, play games, and install applications to make money online daily. 
+                                On Freebyz, you can hire online workers to increase your business visibility and organic growth through engagements on Facebook, Instagram, YouTube, TikTok, WhatsApp, and Twitter.
                                 <br>
-                                Click <a href="{{ url('register') }}">here</a> to Get started and start making cool cash.
+                                <br>SIX ways TO EARN IN DOLLARS AND NAIRA on Freebyz
+                                <br>1. You can do remote jobs by completing tasks and earning. As a verified user, you have access to limitless jobs above N10
+                                <br>2. You can earn dollars on Freebyz (when you have a verified dollar wallet)
+                                <br>3. You can invite your friends using your referral link and earn N500 (or $2.5) on each verified friend. Some users have cashed out more than N100,000 at a time by just referring friends and not completing tasks at all.
+                                <br>4. You can earn more by doing dollar jobs and earn directly to your Paypal account or to your local bank (for the dollar equivalent). 
+                                <br>Feel free to use any of these methods to earn big time on Freebyz.
+                                <br>5. You can promote your Affiliate links to earn big money online. 
+                                <br>6. You can lock money  (to avoid spending temptations) in your wallet for 3-6 months and earn up to 5%  interest.
+
+                                <br>Enjoy Life with Freebyz
+                                <br>To get started, Register at www.freebyz.com then click on your dashboard to start doing jobs and earning
+                                
+                                <br>
+                                Click <a href="{{ url('register') }}"><strong>here</strong></a> to Get started and start making cool cash.
                             </p>
                         </div>
                     </div>
@@ -108,4 +118,56 @@
                 </div>
             </div>
         </div>
+
+        <div class="basic-contact-area pt-90 pb-50">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 col-md-12 col-lg-12 mb-40">
+						<div class="contact-person">
+							<h4>How user made millions on Freebyz</h4>
+							<p>
+                                Last year (2023), a lot of our users became millionaires just by using FREEBYZ and we cannot keep the secrets they used away from you anymore!
+
+                                You can be a millionaire this month as well. 
+
+                                <br>WHAT'S THE SECRET?: <i><b>Your referral Link</b></i>
+
+                                <br>Did you know you are sitting and living with potential millions every day? 
+
+                                <br>A lot of your Friends (on the same WhatsApp group (old friends, church, mosque, association, etc), Telegram group and Facebook/Twitter/TikTok have not heard about Freebyz and you can make money (through them) just by telling them about Freebyz.
+
+                                <br>You will be surprised that you belong to more than 10 WhatsApp groups that have at least 100 people each. 
+
+                                You are sitting on 1000 persons, Chai 
+
+                                Right now, copy your referral link and target 100 people in each WhatsApp group,
+
+                                <br>Go to their DMs and tell them about Freebyz, and show them your evidence of withdrawing from Freebyz. You can add them to a WhatsApp/Facebook group to teach them about Freebyz, Give the training a catchy Title.
+
+                               <strong> Make it Free to attend!</strong>
+
+                               <br>Be Intentional and serious about it, 
+
+                               <br>Guide them to register and verify. You will get up to 1000 people this MONTH OF MARCH  and that is 500k for you. If you target another 1000 on Youtube/Facebook/Instagram, you will get another 500k NEXT MONTH.
+
+                               <br>It will be LOUD if you refer friends who can verify their dollar wallets (Imagine getting $1.5 referral bonus on 1000 persons)
+
+                               <br>Don't PANIC, You're not losing anything by trying!
+
+                                <br>You may just be on your own if you don't try
+
+                                <br> Copy your referral link now and stop sitting on MONEY!
+
+                                Good Luck
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+               
+        
+            </div>
+        </div>
+
+
 @endsection
