58616caaec585cb030c6b39c644e29e7c4fd4a5e
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-07-04 21:20:50 +0100

    fixed 419 page

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('You stayed too Long'))
+@section('code', 'opps, you stayed too long on this page, please refresh. Thanks :)')
+@section('message', __('You stayed too Long'))
