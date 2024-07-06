3b63c409e6d1605110d4b14c9bee72028093c74b
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-07-06 02:44:23 +0100

    deploy error page

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('You stayed too Long'))
-@section('code', 'Refresh Page Please')
-@section('message', __('You stayed too Long'))
+@section('title', __(''))
+@section('code', 'Refresh the page. Thank you :)')
+@section('message', __(''))
