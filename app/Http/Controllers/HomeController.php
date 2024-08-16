7adbb0ab1ee64b1d23420093d637333aa9c22c94
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-08-16 18:33:30 +0100

    jhuiol

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -139,7 +139,7 @@ class HomeController extends Controller
         $withdrawal = Withrawal::get(['status', 'amount', 'is_usd', 'created_at']); //Date('')
         $thisWeekPayment = $withdrawal->where('status', false)->whereBetween('created_at', [$start_week, $end_week])->sum('amount');
         $totalPayout = $withdrawal->where('is_usd', false)->sum('amount');
-        $transactions = PaymentTransaction::where('status', 'successful')->sum('amount');
+        // $transactions = PaymentTransaction::where('status', 'successful')->sum('amount');
         $available_jobsCount = count(availableJobs());
 
         //$ref_rev = Referral::where('is_paid', true)->count();
@@ -182,7 +182,7 @@ class HomeController extends Controller
             'wallet' => $wallet,
             'weekPayment' => $thisWeekPayment,
             'totalPayout' => $totalPayout,
-            'transactions' => $transactions,
+            // 'transactions' => $transactions,
             // 'xmas' => $christmas,
             'av_count' => $available_jobsCount
         ]); // ['users' => $user, 'campaigns' => $campaigns, 'workers' => $campaignWorker, 'loginPoints' => $loginPoints]) // 'wallet' => $wallet, 'ref_rev' => $ref_rev, 'tx' => $transactions, 'wal'=>$Wal])
