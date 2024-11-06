811b67f715cb5670edeb1de052f83f3d7682cea4
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-11-06 20:44:05 +0000

    dsjflbdhgbdf

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Currency extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['code', 'country', 'min_upgrade_amount', 'upgrade_fee', 'allow_upload', 'priotize', 'referral_commission'];
 }
