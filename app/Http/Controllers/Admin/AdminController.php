b0e215308e469b035a65162f0b10ebbc67e9766f
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2025-01-09 14:30:25 +0000

    jhjbh

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -394,7 +394,7 @@ class AdminController extends Controller
         return view('admin.admin_transactions', ['lists' => $list]);
     }
     public function userTransaction(){
-        $list = PaymentTransaction::where('user_type', 'regular')->where('status', 'successful')->orderBy('created_at', 'DESC')->get();
+        $list = PaymentTransaction::where('user_type', 'regular')->where('status', 'successful')->orderBy('created_at', 'DESC')->paginate(50);
         return view('admin.users.user_transactions', ['lists' => $list]);
     }
 
