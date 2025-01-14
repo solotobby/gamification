54661ad76a6209fb87516634c7a073a4d242f1d8
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2025-01-14 10:16:39 +0000

    done

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,8 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'title', 'description', 'status'];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
