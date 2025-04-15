c901c2bb328a3e7ece9097872c0923ab3e3c8584
Eclatspad <eclatspad@gmail.com>
2025-04-15 14:22:17 +0100

    deploy info

--- app/Http/Controllers/Admin/FraudController.php
+++ app/Http/Controllers/Admin/FraudController.php
@@ -15,19 +15,12 @@ class FraudController extends Controller
     }
 
     public function removeDuplicateAccount(){
-        $duplicateAccountNumbers = DB::table('account_information as a')
+        $duplicateAccountNumbers = DB::table('bank_information as a')
             ->join('users as u', 'a.user_id', '=', 'u.id')
             ->select('a.account_number', 'u.name', DB::raw('COUNT(*) as total'))
             ->groupBy('a.account_number', 'u.name')
             ->havingRaw('COUNT(*) > 1')
             ->get();
-        // DB::table('account_information')
-        //     ->select('account_number')
-        //     ->groupBy('account_number')
-        //     ->havingRaw('COUNT(*) > 1')
-        //     ->get();
-            // ->pluck('account_number');
-
         return view('admin.fraud.duplicate', ['duplicates' => $duplicateAccountNumbers]);
     }
 }
