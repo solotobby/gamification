a200c9bcb7b108c3eb828e432ffb1e2ae924085b
Eclatspad <eclatspad@gmail.com>
2025-05-02 20:56:30 +0100

    doings

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -162,9 +162,9 @@ class HomeController extends Controller
         return view('user.documentation.how_to_approve');
     }
 
-    // public function adminNewHome(){
-    //     return view('admin.new_index');
-    // }
+    public function adminNewHome(){
+        return view('admin.new_index');
+    }
 
     public function adminHome()
     {
