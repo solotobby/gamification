2735ca1b95d79a7b4ca5140f062926d04e84d664
Eclatspad <eclatspad@gmail.com>
2025-05-12 21:08:23 +0100

    deploy new updates

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -844,7 +844,7 @@ class AdminController extends Controller
         // ->orderBy('created_at', 'DESC')->paginate(200);
 
      
-        $yesterday = Carbon::yesterday();
+        $yesterday = Carbon::today()->subDays(15); //Carbon::yesterday();
         $list =  CampaignWorker::where('status', 'Pending')->where('reason', null)
                ->whereDate('created_at', $yesterday)
                ->paginate(50);
