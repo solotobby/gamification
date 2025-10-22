816043f42b2ea269a623a6bb5218dccc175b8e20
Victor Morakinyo <morakinyovictor1@gmail.com>
2025-10-22 13:30:16 +0100

    Integrate KoraPay Webhook

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -212,8 +212,8 @@ class WalletController extends Controller
                         "name" => auth()->user()->name,
                         "email" => auth()->user()->email
                     ],
-                    "notification_url" => config('services.env.env' === 'Production') ? route('koraPay.webhook') : "https://webhook.site/d9458b90-f6bb-4775-9f58-eb8bebdca5b7",
-                    // "notification_url" =>  route('koraPay.webhook'),
+                    // "notification_url" => config('services.env.env' === 'Production') ? route('koraPay.webhook') : "https://webhook.site/d9458b90-f6bb-4775-9f58-eb8bebdca5b7",
+                    "notification_url" =>  route('koraPay.webhook'),
                     // "metadata"=>[
                     //     "key0"=> "test0",
                     //     "key1"=> "test1",
