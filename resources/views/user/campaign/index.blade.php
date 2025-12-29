940bad36cb7264f8c68c806f0b48213b5ad191b8
Victor Morakinyo <morakinyovictor1@gmail.com>
2025-12-29 23:12:51 +0100

    Add more workers

--- resources/views/user/campaign/index.blade.php
+++ resources/views/user/campaign/index.blade.php
@@ -22,13 +22,13 @@
         <div class="alert alert-warning">
             Campaigns with Activity Status <b>Completed</b> will not be available on the dashboard.
         </div>
-             <div class="alert alert-warning d-flex align-items-center mb-4" role="alert">
-        <i class="fa fa-exclamation-triangle me-2"></i>
-        <span>
-           To ensure fairness, kindly avoid denying task submissions without genuine reasons.
-           Repeated denials will lead to your campaigns being suspended without refund.
-        </span>
-    </div>
+        <div class="alert alert-warning d-flex align-items-center mb-4" role="alert">
+            <i class="fa fa-exclamation-triangle me-2"></i>
+            <span>
+                To ensure fairness, kindly avoid denying task submissions without genuine reasons.
+                Repeated denials will lead to your campaigns being suspended without refund.
+            </span>
+        </div>
         <div class="block block-rounded">
             <div class="block-header block-header-default">
                 <h3 class="block-title">Campaign List</h3> <small><span style="color: chocolate"></span></small>
@@ -67,144 +67,71 @@
                         </thead>
                         <tbody>
                             @foreach ($lists as $list)
-                                <?php
+                                                    <?php
                                 $campaignStat = checkCampaignCompletedStatus($list->id);
-                                ?>
-                                <tr>
-                                    <td>
-                                        {{ $list->job_id }}
-                                    </td>
-                                    <td>
-                                        {{ $list->post_title }}
-                                    </td>
-                                    <td>
-                                        {{ @$campaignStat['counts']['Approved'] }}/{{ $list->number_of_staff }}
-                                        {{-- {{ $list->completed()->where('status', '=', 'Approved')->count(); }}/{{
+                                                            ?>
+                                                    <tr>
+                                                        <td>
+                                                            {{ $list->job_id }}
+                                                        </td>
+                                                        <td>
+                                                            {{ $list->post_title }}
+                                                        </td>
+                                                        <td>
+                                                            {{ @$campaignStat['counts']['Approved'] }}/{{ $list->number_of_staff }}
+                                                            {{-- {{ $list->completed()->where('status', '=', 'Approved')->count(); }}/{{
                                                             $list->number_of_staff }} --}}
-                                    </td>
-                                    <td>
-                                        {{-- @if ($list->currency == 'NGN')
+                                                        </td>
+                                                        <td>
+                                                            {{-- @if ($list->currency == 'NGN')
                                                             &#8358;{{ $list->campaign_amount }}
                                                             @else
                                                             ${{ $list->campaign_amount }}
                                                             @endif --}}
-                                        {{ baseCurrency() }}
-                                        {{ currencyConverter($list->currency, baseCurrency(), $list->campaign_amount) }}
+                                                            {{ baseCurrency() }}
+                                                            {{ currencyConverter($list->currency, baseCurrency(), $list->campaign_amount) }}
 
-                                    </td>
-                                    <td>
-
-
-                                        {{ baseCurrency() }}
-                                        {{ currencyConverter($list->currency, baseCurrency(), $list->total_amount) }}
-                                    </td>
+                                                        </td>
+                                                        <td>
 
 
-                                    <td>{{ $list->status }}</td>
-                                    <?php
-                                    // $c = $campaignStat['Pending']  + $campaignStat['Approved'] ;
-                                    ?>
-                                    <td>
-                                        {{ $list->is_completed ? 'Completed' : 'Not Completed' }}
-                                        {{-- {{ $c >= $list->number_of_staff ? 'Completed' : 'Not Completed' }} --}}
-                                    </td>
-
-                                    {{-- <td>{{ $list->allow_upload == true ? 'Yes' : 'No' }}</td> --}}
-                                    <td>
-                                        @if ($list->status == 'Offline' || $list->status == 'Flagged')
-                                            {{-- <a href="#" class="btn btn-primary btn-sm disabled"> Go Live </a> --}}
-                                        @else
-                                            <a href="{{ url('campaign/activities/' . $list->job_id) }}"
-                                                class="btn btn-success btn-sm"> View Activities </a>
-                                            <a href="{{ url('campaign/activities/pause/' . $list->job_id) }}"
-                                                class="btn btn-warning btn-sm"> Pause Campaign </a>
-                                        @endif
-
-                                        @if(@$list->user->is_business)
-                                         <a type="button" class="btn btn-alt-primary btn-sm"
-                                            href="{{ url('campaign/' . $list->job_id .'/extraworker') }}">
-                                                Add More Workers
-                                        </a>
-                                        @endif
-
-                                        {{-- <button type="button" class="btn btn-alt-primary btn-sm" data-bs-toggle="modal"
-                                                                data-bs-target="#modal-default-popout-{{ $list->job_id }}">
-                                                                Add More Workers
-                                                            </button> --}}
-                                    </td>
-                                </tr>
+                                                            {{ baseCurrency() }}
+                                                            {{ currencyConverter($list->currency, baseCurrency(), $list->total_amount) }}
+                                                        </td>
 
 
-                                <!-- Pop Out Default Modal -->
-                                {{-- <div class="modal fade" id="modal-default-popout-{{ $list->job_id }}" tabindex="-1"
-                                    role="dialog" aria-labelledby="modal-default-popout" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-popout" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title"> Add More Worker </h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
-                                            </div>
+                                                        <td>{{ $list->status }}</td>
+                                                        <?php
+                                // $c = $campaignStat['Pending']  + $campaignStat['Approved'] ;
+                                                                ?>
+                                                        <td>
+                                                            {{ $list->is_completed ? 'Completed' : 'Not Completed' }}
+                                                            {{-- {{ $c >= $list->number_of_staff ? 'Completed' : 'Not Completed' }} --}}
+                                                        </td>
 
-                                            <div class="modal-body pb-1">
-                                                Current Number of Workers - {{ $list->number_of_staff }} <br>
-                                                Value per Job - {{ baseCurrency() }}
-                                                {{ currencyConverter($list->currency, baseCurrency(), $list->campaign_amount) }}<br>
-                                                Total Value of Job - {{ baseCurrency() }}
-                                                {{ currencyConverter($list->currency, baseCurrency(), $list->total_amount) }}
-                                                <br>
+                                                        {{-- <td>{{ $list->allow_upload == true ? 'Yes' : 'No' }}</td> --}}
+                                                        <td>
+                                                            @if ($list->status == 'Offline' || $list->status == 'Flagged')
+                                                                <!-- Disabled state -->
+                                                            @else
+                                                                <a href="{{ url('campaign/activities/' . $list->job_id) }}"
+                                                                    class="btn btn-success btn-sm">
+                                                                    View Activities
+                                                                </a>
+                                                                <a href="{{ url('campaign/activities/pause/' . $list->job_id) }}"
+                                                                    class="btn btn-warning btn-sm">
+                                                                    Pause Campaign
+                                                                </a>
+                                                            @endif
 
+                                                            <a href="{{ route('campaign.add.worker', $list->job_id) }}"
+                                                                class="btn btn-alt-primary btn-sm">
+                                                                <i class="fa fa-plus me-1"></i> Add More Workers
+                                                            </a>
 
+                                                        </td>
+                                                    </tr>
 
-                                                <hr>
-                                                <form action="{{ route('addmore.workers') }}" method="POST">
-                                                    @csrf
-                                                    <div class="mb-4">
-                                                        <label class="form-label" for="post-files">Number of
-                                                            Worker</small></label>
-                                                        <input class="form-control" name="new_number" id="staff"
-                                                            type="number" required>
-                                                    </div>
-                                                    <input type="hidden" name="id" value="{{ $list->job_id }}">
-                                                    <input type="text" name="amount"
-                                                        value="{{ currencyConverter($list->currency, baseCurrency(), $list->campaign_amount) }}">
-
-                                                    <input type="text" name="wallet_balance" id="wallet_balance"
-                                                        value="{{ checkWalletBalance($user, $user->wallet->base_currency, $list->campaign_amount) }}">
-
-                                                    <input type="text" name="campaign_amount" id="campaignAmount"
-                                                        value="{{ $list->campaign_amount }}">
-
-                                                    <input type="text" name="currency" id="currency"
-                                                        value="{{ baseCurrency() }}">
-
-                                                    <input type="text" id="total" name="total" value="0">
-
-                                                    <p id="totalDisplay" class="mb-2 fw-bold">Total: {{ baseCurrency() }} 0
-                                                    </p>
-
-                                                    <div class="mb-4">
-                                                        <button class="btn btn-secondary" id="addBtn" type="submit">Add
-                                                            More
-                                                            Worker</button>
-                                                    </div>
-
-                                                    <p id="message" class="text-danger mt-2" style="display:none;"></p>
-
-                                                </form>
-
-
-                                            </div>
-
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-sm btn-alt-secondary"
-                                                    data-bs-dismiss="modal">Close</button>
-                                                {{-- <button type="submit" class="btn btn-sm btn-primary"
-                                                                        data-bs-dismiss="modal">Done</button> --}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> --}}
                             @endforeach
 
                         </tbody>
