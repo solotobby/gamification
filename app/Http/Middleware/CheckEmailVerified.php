181ff1136398681eaf6a87986e645925ba3573bf
Victor Morakinyo <morakinyovictor1@gmail.com>
2026-04-02 16:18:31 +0100

    enforce block user

--- app/Http/Middleware/CheckEmailVerified.php
+++ app/Http/Middleware/CheckEmailVerified.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,13 +11,27 @@ class CheckEmailVerified
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->check() && is_null(auth()->user()->email_verified_at)) {
-            // Allow access to verification routes
-            if ($request->routeIs('verification.*')) {
-                return $next($request);
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            // Blacklisted user
+            if ($user->is_blacklisted) {
+                Auth::logout();
+
+                return redirect()->route('login')
+                    ->withErrors(['email' => 'Your account has been blocked.']);
             }
 
-            return response()->view('auth.email_verification');
+            // Email not verified
+            if (is_null($user->email_verified_at)) {
+
+                // Allow verification routes
+                if ($request->routeIs('verification.*')) {
+                    return $next($request);
+                }
+
+                return response()->view('auth.email_verification');
+            }
         }
 
         return $next($request);
