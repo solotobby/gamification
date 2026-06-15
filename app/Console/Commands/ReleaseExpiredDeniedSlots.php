51dc5f3cd0f4545f592444d212fe98f1425d605d
Victor Morakinyo <morakinyovictor1@gmail.com>
2026-06-15 10:46:39 +0100

    supervisor

--- app/Console/Commands/ReleaseExpiredDeniedSlots.php
+++ app/Console/Commands/ReleaseExpiredDeniedSlots.php
@@ -51,6 +51,7 @@ class ReleaseExpiredDeniedSlots extends Command
                 $campaign = Campaign::find($deniedWork->campaign_id);
                 if ($campaign && $campaign->pending_count > 0) {
                     $campaign->pending_count += 1;
+                    $campaign->is_complete = false;
                     $campaign->save();
 
                     $this->info("Released slot for Campaign ID: {$campaign->id}, Worker ID: {$deniedWork->id}");
